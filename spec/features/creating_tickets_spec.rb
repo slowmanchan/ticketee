29f703e1a484ed848ddb7f95c6e05eb90cccba25
Norman Chan <slowmanchan@gmail.com>
2016-11-17 14:21:36 -0500

    File uploads are persisted and redisplays

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -12,6 +12,19 @@ RSpec.feature "Users can create new tickets" do
     click_link "New Ticket"
   end
 
+  scenario "persisting file uploads across from displays" do
+    attach_file "File", "spec/fixtures/speed.txt"
+    click_button "Create Ticket"
+
+    fill_in "Name", with: "Add documentation for blink tag"
+    fill_in "Description", with: "The blink tag has a speed attribute"
+    click_button "Create Ticket"
+
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
+    end
+  end
+
   scenario "with an attachment" do
     fill_in "Name", with: "Add documentation for blink tag"
     fill_in "Description", with: "The blink tag has a speed attribute"
@@ -24,7 +37,7 @@ RSpec.feature "Users can create new tickets" do
       expect(page).to have_content "speed.txt"
     end
   end
-  
+
   scenario "with valid attributes" do
     fill_in "Name", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
