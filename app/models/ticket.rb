e8016db02c229591fa1413cff1e8fb4d08e069f8
Norman Chan <slowmanchan@gmail.com>
2016-11-23 18:25:11 -0500

    Tickets can have a state assigned when comments are created

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :project
+  belongs_to :state
+  
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
 
